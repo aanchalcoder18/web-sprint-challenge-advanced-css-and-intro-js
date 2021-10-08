38befa9dd26aacbd0f7d1c6a9a4dab8b075f4df0
aanchalcoder18 <aasaxena14@gmail.com>
2021-10-08 10:11:34 -0700

    task 5 and 6

--- index.js
+++ index.js
@@ -269,9 +269,12 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array.length;
 }
+
+console.log(removeArtist(artists, 0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -290,10 +293,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, id, name, years, genre, nationality, bio) {
+  array.push({id,name, years, genre, nationality, bio});
+  return array;
 }
-
+console.log(addArtist(artists,'20', 'Aanchal', '1997-2021', 'Full Stack', 'Indian', 'My hobbies are swimming, reading, and watching marvel movies'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
